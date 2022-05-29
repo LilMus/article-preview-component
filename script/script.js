2ec313a64cc966cfe6c0756531af849a4261c55c
LilMus <mustapha.seghir@gmail.com>
2022-05-29 21:00:00 +0200

    final version without hovering effects

--- script/script.js
+++ script/script.js
@@ -1,16 +1,46 @@
-const buttonArr = document.getElementsByClassName("button");
-const button = buttonArr[0];
+const button = document.getElementsByClassName("button")[0];
+const socials = document.getElementsByClassName("socials")[0];
+const arrow = document.getElementsByClassName("arrow")[0];
+const author = document.getElementsByClassName("author")[0];
 
-function setClickedTrue() {
-	button.setAttribute("isClicked", true);
+function setClickedTrue(element) {
+	element.setAttribute("isClicked", true);
 }
 
-function setClickedFalse() {
-	button.setAttribute("isClicked", false);
+function setClickedFalse(element) {
+	element.setAttribute("isClicked", false);
+}
+
+function displaySocials() {
+	socials.style.display = "flex";
+	arrow.style.display = "inline-block";
+	if (window.screen.width < 1300) {
+		author.style.display = "none";
+		button.style = `
+			position : absolute;
+			right: 0;
+			top : 20%;
+		`;
+	}
+}
+
+function hideSocials() {
+	socials.style.display = "none";
+	arrow.style.display = "none";
+	if (window.screen.width < 1300) {
+		author.style.display = "flex";
+		button.style.position = "static";
+	}
 }
 
 button.addEventListener("click", function () {
-	setClickedTrue();
+	if (button.getAttribute("isClicked") === "true") {
+		setClickedFalse(button);
+		hideSocials();
+		return;
+	}
+	setClickedTrue(button);
+	displaySocials();
 });
 
-setClickedFalse();
+setClickedFalse(button);
